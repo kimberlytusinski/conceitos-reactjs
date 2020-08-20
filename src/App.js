3ef6320cf6074b4c8c6099486ed742a2ff64e90c
KimberlyTusinski <kimberly.tusinski@hotmail.com>
2020-08-20 17:34:11 -0300

    Starting ReactJS

--- src/App.js
+++ src/App.js
@@ -1,31 +1,52 @@
-import React from "react";
+import React from 'react'
+import { useState, useEffect } from 'react'
+import api from './services/api'
 
-import "./styles.css";
+import './styles.css'
 
 function App() {
-  async function handleAddRepository() {
-    // TODO
-  }
+	const [repositories, setRepositories] = useState([])
 
-  async function handleRemoveRepository(id) {
-    // TODO
-  }
+	useEffect(() => {
+		api.get('repositories').then(response => {
+			setRepositories(response.data)
+		})
+	}, [])
 
-  return (
-    <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+	async function handleAddRepository() {
+		const response = await api.post('repositories', {
+			title: `New repository ${Date.now()}`,
+			url: 'https://github.com/KimberlyTusinski',
+			techs: ['Node.js', 'React', 'VueJS']
+		})
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-      </ul>
+		const repository = response.data
+		setRepositories([...repositories, repository])
+	}
 
-      <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
-  );
+	async function handleRemoveRepository(id) {
+		await api.delete(`repositories/${id}`).then(
+			setRepositories(repositories.filter(repository => repository.id != id))
+		)
+	}
+
+	return (
+		<div>
+			<ul data-testid="repository-list">
+			{repositories.map(repository => (
+				<li key={repository.id}>
+					{repository.title}           
+
+					<button onClick={() => handleRemoveRepository(repository.id)}>
+						Remover
+					</button>
+				</li> 
+				))
+			}
+			</ul>			
+			<button onClick={handleAddRepository}>Adicionar</button>
+		</div>
+	)
 }
 
-export default App;
+export default App
